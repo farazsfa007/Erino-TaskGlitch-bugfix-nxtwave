1fd0fd01a807df3329e0f9c8fec793adcd0c7d79
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2026-01-12 15:05:10 +0530

    Fix vite config types and dirname for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import path from 'node:path';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
-
