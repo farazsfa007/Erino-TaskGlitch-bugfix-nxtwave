3e557264ad69c22120c4efb5bb44a2000f09fc16
Syed Faraz Ahmad <farazahmadsfa@gmail.com>
2026-01-12 15:08:29 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 var __filename = fileURLToPath(import.meta.url);
 var __dirname = path.dirname(__filename);
 export default defineConfig({
+    plugins: [react()],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-    plugins: [react()]
 });
